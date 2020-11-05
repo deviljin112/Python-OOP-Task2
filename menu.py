78d0acc64383a0516c71d977f097b90e259d60e4
D3v <deviljin112@users.noreply.github.com>
2020-11-05 15:34:03 +0000

    Readme + Pseudo

--- menu.py
+++ menu.py
@@ -1,5 +1,8 @@
+# Creates parent class
 class Menu:
+    # Initialises the parent class
     def __init__(self):
+        # Only variable in this class is the Menu
         self.menu = [
             "Spicy Chicken",
             "Sweat and Sour Beef",
@@ -15,31 +18,46 @@ class Menu:
         ]
 
 
+# Creates a child class of parent "Menu"
 class Waiter(Menu):
     def __init__(self):
+        # Imports the variables from parent
         super().__init__()
+        # Creates a new child-specific variable
         self.order = []
 
+    # Class function for ordering from the menu
     def ordering(self):
-        is_ordering = True
+        # First iteration prompt
         print("What would you like to order?")
 
+        # Boolean for looping
+        is_ordering = True
+
         while is_ordering:
+            # Takes input
             choice = input("=> ")
 
+            # Checks that this item is available in the menu
             if choice in self.menu:
+                # If yes then adds to the order
                 self.order.append(choice)
             else:
                 print("We do not offer that type of food")
 
             print("Anything else?")
 
+            # Asks user if they would like to add more items
             choice_2 = input("=> ")
             if choice_2.lower() == "no":
+                # If no, breaks the loop
                 is_ordering = False
+            else:
+                print("What else would you like?")
 
         return self.order
 
+    # This is used for clean formatting when showing the self.order table
     def __str__(self):
         return " ".join(k for k in self.order)
 
